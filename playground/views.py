4388bd5991b2a44d3066d06a0840e7b1172500bf
Showrin <showrinece15@gmail.com>
2022-07-14 07:48:58 +0600

    :sparkles: Add a view and template in playground/hello endpoint

--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,9 @@
+import imp
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def say_hello(request):
+    x = 1
+    y = 2
+    return render(request, 'hello.html', { 'name': 'Showrin'})
